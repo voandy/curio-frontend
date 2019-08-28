8cc951bcaad9170146c4d91e90efa7fe13250b84
nickangmc <nickang.mc@hotmail.com>
2019-08-28 15:59:41 +1000

    Shaun's Minor Bug Fixed
    1. "BorderBottomSthg" does not exist apparently.
    2. Changed it to something that exists.

--- src/scenes/Collections/index.js
+++ src/scenes/Collections/index.js
@@ -3,47 +3,39 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import {
-    View,
-    ScrollView,
-    StyleSheet,
-    Text,
-    Dimensions,
-    TouchableOpacity,
-  } from 'react-native'
+  View,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
 
 import { white, black } from "ansi-colors";
-import CardCarousel from "../../component/CardCarousel"
-import Header from "../../component/Header"
-
+import CardCarousel from "../../component/CardCarousel";
+import Header from "../../component/Header";
 
 class Collections extends Component {
-
   render() {
     const { user } = this.props.auth;
     console.log("user is" + user);
 
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
+        <Header title="Collections" tab1="Public" tab2="Private" />
 
-          <Header title="Collections" tab1="Public" tab2="Private"/>         
-
-          {/* carousel pinned collections */}
-          <View style={{height: 130, marginTop: 20}}>
-            <ScrollView  
-            horizontal={ true }
-            showsHorizontalScrollIndicator={ false }
-            >
-              <CardCarousel text="page 1"/>
-              <CardCarousel text="page 2"/>
-              <CardCarousel text="page 3"/>
-
-            </ScrollView>
-          </View>
-
-          <Text style = {styles.titleText}>COLLECTIONS</Text>
-
+        {/* carousel pinned collections */}
+        <View style={{ height: 130, marginTop: 20 }}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <CardCarousel text="page 1" />
+            <CardCarousel text="page 2" />
+            <CardCarousel text="page 3" />
+          </ScrollView>
         </View>
-      )
+
+        <Text style={styles.titleText}>COLLECTIONS</Text>
+      </View>
+    );
   }
 }
 
@@ -56,63 +48,58 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
 
   header: {
     height: 130,
-    backgroundColor: white,
     elevation: 2,
-    borderBottomColor: black,
     borderRadius: 1
-  },  
+  },
 
-  headerText : {
+  headerText: {
     fontSize: 24,
     marginTop: 40,
     marginLeft: 30,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
 
   headerButton: {
-    alignContent: 'center',
+    alignContent: "center",
     marginTop: 20,
     marginLeft: 30,
     padding: 5,
-    borderColor:'#FF6E6E',
+    borderColor: "#FF6E6E",
     borderWidth: 0,
     borderBottomWidth: 3,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
 
   headerButtonText: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
 
   mainCollectionContainer: {
-    height: Dimensions.get('window').height * 0.3,
+    height: Dimensions.get("window").height * 0.3,
     top: 0,
-    position: 'absolute',
-    backgroundColor: '#E2E2E2',
-  },  
+    position: "absolute",
+    backgroundColor: "#E2E2E2"
+  },
 
   titleText: {
     fontSize: 30,
     marginTop: 100,
-    fontWeight: 'bold',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: Dimensions.get('window').width * 0.07,
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: Dimensions.get("window").width * 0.07
     // marginBottom: 50,
     // fontFamily: 'HindSiliguri-Bold'
-  },
-
+  }
 });
-
 
 //  export
 export default connect(
