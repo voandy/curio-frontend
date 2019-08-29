9937387652903d492ce0e6559a924cb73d607c89
Ze Shaun Soong <z.soong@student.unimelb.edu.au>
2019-08-29 15:07:18 +1000

    added icons in tab bar

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -11,6 +11,12 @@ import NotificationScreen from './Notification'
 import ArtifactsScreen from './Artifacts'
 import { white } from 'ansi-colors';
 
+import {
+	Image
+  } from 'react-native'
+
+const defaultTintColor = '#E2E2E2';
+
 // login / signup stack
 const AuthStack = createStackNavigator(
 	{ 
@@ -22,28 +28,52 @@ const AuthStack = createStackNavigator(
 // default app stack
 const AppStack = createBottomTabNavigator(
 	{
-		Collections: {screen: CollectionsScreen},
-		Artifacts: {screen: ArtifactsScreen},
-		Notification: {screen: NotificationScreen},
-		Profile: {screen: ProfileScreen},
+		Collections: {
+			screen: CollectionsScreen,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor}) => (
+					<Image source={ require('../../assets/images/icons/collection.png') } 
+					style = {{ height:24, width:24, tintColor:defaultTintColor}}/>
+				)
+			}},
+		Artifacts: {
+			screen: ArtifactsScreen,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor}) => (
+					<Image source={ require('../../assets/images/icons/artefact.png') } 
+					style = {{ height:24, width:30, tintColor:defaultTintColor}}/>
+				)
+			}},
+		Notification: {
+			screen: NotificationScreen,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor}) => (
+					<Image source={ require('../../assets/images/icons/notification.png') } 
+					style = {{ height:24, width:30, tintColor:defaultTintColor }}/>
+				)
+			}},
+		Profile: {
+			screen: ProfileScreen,
+			navigationOptions: {
+				tabBarIcon: ({ tintColor}) => (
+					<Image source={ require('../../assets/images/icons/profile.png') } 
+					style = {{ height:24, width:24, tintColor:defaultTintColor }}/>
+				)
+			}},
 	},
 	// styling for the tab bar
 	{
 		tabBarOptions:{
 			activeTintColor: '#FF6E6E',
 			inactiveTintColor: '#E2E2E2',
+			showLabel: false,
 			style: {
 				elevation: 1,
 				backgroundColor: white,
 				borderTopWidth: 0,
 				height: 60,
 				
-			},
-			labelStyle: {
-				fontSize: 10,
-				marginBottom: 10,
-
-			  },		
+			},		
 		}
 	}
 	);
