0b46ad176304aa1d7f468bc91ac7514706012fa2
nickangmc <nickang.mc@hotmail.com>
2019-09-09 18:43:56 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
 import React from "react";
 import Provider from "./src/provider";
 import * as Font from "expo-font";
-import { connect } from "react-redux";
-import { loadFont } from "./src/actions/loadFontActions";
+// import { connect } from "react-redux";
+// import { loadFont } from "./src/actions/loadFontActions";
 
-class App extends React.Component {
-  // state = {
-  //   fontLoaded: false
-  // };
+export default class App extends React.Component {
+  // // load fonts TODO
+  // async componentDidMount() {
+  //   await Font.loadAsync({
+  //     "HindSiliguri-Bold": require("./assets/fonts/HindSiliguri-Bold.ttf"),
+  //     "HindSiliguri-Light": require("./assets/fonts/HindSiliguri-Light.ttf"),
+  //     "HindSiliguri-Regular": require("./assets/fonts/HindSiliguri-Regular.ttf")
+  //   });
 
-  // load fonts TODO
-  async componentDidMount() {
-    await Font.loadAsync({
-      "HindSiliguri-Bold": require("./assets/fonts/HindSiliguri-Bold.ttf"),
-      "HindSiliguri-Light": require("./assets/fonts/HindSiliguri-Light.ttf"),
-      "HindSiliguri-Regular": require("./assets/fonts/HindSiliguri-Regular.ttf")
-    });
-
-    // this.setState({ fontLoaded: true });
-    this.props.loadFont();
-  }
+  //   // this.setState({ fontLoaded: true });
+  //   this.props.loadFont();
+  // }
 
   render() {
     return <Provider />;
   }
 }
 
-export default connect(
-  null,
-  { loadFont }
-)(App);
+// export default connect(
+//   null,
+//   { loadFont }
+// )(App);
