2912b2b3655138914b589ccfd6e7909ed9f02266
csugianto <csugianto@student.unimelb.edu.au>
2019-09-20 14:29:03 +1000

    setup redux for selecting group

--- src/types/groupsTypes.js
+++ src/types/groupsTypes.js
@@ -1,3 +1,4 @@
 export const SET_USER_GROUPS = "SET_USER_GROUPS";
 export const ADD_NEW_GROUP = "ADD_NEW_GROUP";
 export const GET_ERRORS = "GET_ERRORS";
+export const SET_SELECTED_GROUP = "SET_SELECTED_GROUP";
