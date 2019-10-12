3555dc019f78dfb8f20093c84f106c5b0ce76261
nickangmc <nickang.mc@hotmail.com>
2019-10-12 22:02:59 +1100

    Fixed button out-of-place issue + minor design tweaks
    1. The position: "absolute" rule on addbutton was not working, adding a parent view with absolute position fixed the issue.
    2. Made the button's size responsive + lower the elevation (shadow).

--- src/component/AddButton.js
+++ src/component/AddButton.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
-import { StyleSheet, TouchableOpacity, Text, Animated } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Animated,
+  View
+} from "react-native";
 
 // respondsive design components
-import { deviceWidthDimension as wd } from "../utils/responsiveDesign";
+import {
+  deviceWidthDimension as wd,
+  deviceHeigthDimension as hp
+} from "../utils/responsiveDesign";
 
 /** general add button
  *  used in groups, artefacts and selected groups pages */
@@ -21,24 +30,29 @@ class AddButton extends Component {
 
   render() {
     return (
-      <Animated.View style={this.slideAnimation.getLayout()}>
-        <TouchableOpacity style={styles.add} onPress={this.props.onPress}>
-          <Text style={styles.text}>+</Text>
-        </TouchableOpacity>
-      </Animated.View>
+      <View style={styles.container}>
+        <Animated.View style={this.slideAnimation.getLayout()}>
+          <TouchableOpacity style={styles.add} onPress={this.props.onPress}>
+            <Text style={styles.text}>+</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  add: {
-    bottom: 30, // default standard for material design
-    right: 30,
+  container: {
     position: "absolute",
-    width: 56,
-    height: 56,
-    borderRadius: 56 / 2,
-    elevation: 5,
+    bottom: 30, // default standard for material design
+    right: 30
+  },
+
+  add: {
+    width: hp(0.07),
+    height: hp(0.07),
+    borderRadius: hp(0.07),
+    elevation: 1,
     alignItems: "center",
     alignContent: "center",
     justifyContent: "center",
@@ -46,7 +60,7 @@ const styles = StyleSheet.create({
   },
 
   text: {
-    fontSize: 28,
+    fontSize: hp(0.07) / 1.7,
     color: "white"
   }
 });
