e1e47279c9c18173b446782561f571768c22af52
nickangmc <nickang.mc@hotmail.com>
2019-10-12 23:25:08 +1100

    Fixed user re-logs in data not retrieving issue

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -38,18 +38,39 @@ import { getUserNotifications } from "../actions/notificationActions";
 const { registerForPushNotificationsAsync } = require("../services/notification/registerForPushNotificationsAsync");
 
 class Scenes extends Component {
+  // for scenarios: there's an user stayed logged in on app launch
+  // or there's no user logged in on app launch
   async componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-
       // get user authentication data
       const { user } = this.props.auth;
-
       // get user data and artefacts
       this.props.getUserData(user.id);
       this.props.getUserArtefacts(user.id);
       this.props.getUserGroups(user.id);
       this.props.getUserNotifications(user.id);
+      // post user's expo-push-token to backend if haven't already
+      registerForPushNotificationsAsync(user.id);
+    }
+  }
 
+  // for scenario: an user logs out on the app and tries to log back in
+  async componentDidUpdate(prevProps) {
+    // extract users details from redux states
+    const user = this.props.auth.user;
+    const previousUser = prevProps.auth.user;
+    // a user logs in and there's no previous user data in redux store
+    if (
+      Object.keys(previousUser).length === 0 &&
+      Object.keys(user).length > 0
+    ) {
+      // get user authentication data
+      const { user } = this.props.auth;
+      // get user data and artefacts
+      this.props.getUserData(user.id);
+      this.props.getUserArtefacts(user.id);
+      this.props.getUserGroups(user.id);
+      this.props.getUserNotifications(user.id);
       // post user's expo-push-token to backend if haven't already
       registerForPushNotificationsAsync(user.id);
     }
@@ -67,15 +88,14 @@ const GroupStack = createStackNavigator({
   SelectedGroup: { screen: SelectedGroupScreen },
   SelectedArtefact: { screen: SelectedArtefactScreen },
   GeneralSearch: { screen: GeneralSearchScreen },
-  UserSearch: { screen: UserSearchScreen },
-
+  UserSearch: { screen: UserSearchScreen }
 });
 
 const ArtefactStack = createStackNavigator({
   Artefacts: { screen: ArtefactsScreen },
   ArtefactsForm: { screen: ArtefactsFormScreen },
   SelectedArtefact: { screen: SelectedArtefactScreen },
-  GeneralSearch: { screen: GeneralSearchScreen },
+  GeneralSearch: { screen: GeneralSearchScreen }
 });
 
 const NotificationStack = createStackNavigator({
@@ -83,7 +103,7 @@ const NotificationStack = createStackNavigator({
   SelectedArtefact: { screen: SelectedArtefactScreen },
   SelectedGroup: { screen: SelectedGroupScreen },
   GeneralSearch: { screen: GeneralSearchScreen },
-  Invitation: { screen: InvitationScreen },
+  Invitation: { screen: InvitationScreen }
 });
 
 const ProfileStack = createStackNavigator({
