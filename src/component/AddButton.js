6f5ac761e3eb9e9d7fc6fe36399e4119214f7210
nickangmc <nickang.mc@hotmail.com>
2019-10-12 21:57:08 +1100

    Reformat addButton file

--- src/component/AddButton.js
+++ src/component/AddButton.js
@@ -1,68 +1,54 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    TouchableOpacity,
-    Text,
-    Animated
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, TouchableOpacity, Text, Animated } from "react-native";
 
 // respondsive design components
-import {
-    deviceWidthDimension as wd
-} from "../utils/responsiveDesign";
+import { deviceWidthDimension as wd } from "../utils/responsiveDesign";
 
-/** general add button 
+/** general add button
  *  used in groups, artefacts and selected groups pages */
 class AddButton extends Component {
+  constructor(props) {
+    super(props);
+    this.slideAnimation = new Animated.ValueXY({ x: wd(1), y: 0 });
+  }
 
-    constructor(props) {
-        super(props);
-        this.slideAnimation = new Animated.ValueXY({ x: wd(1), y: 0 });
-    }
+  componentDidMount() {
+    Animated.spring(this.slideAnimation, {
+      toValue: { x: 0, y: 0 },
+      friction: 7
+    }).start();
+  }
 
-    componentDidMount() {
-        Animated.spring(this.slideAnimation, {
-            toValue: { x: 0, y: 0 },
-            friction: 7
-        }).start();
-    }
-
-
-    render() {
-        return (
-            <Animated.View style={this.slideAnimation.getLayout()}>
-                <TouchableOpacity style={styles.add} onPress={this.props.onPress}>
-                    <Text style={styles.text}>+</Text>
-                </TouchableOpacity>
-            </Animated.View>
-
-        )
-    }
+  render() {
+    return (
+      <Animated.View style={this.slideAnimation.getLayout()}>
+        <TouchableOpacity style={styles.add} onPress={this.props.onPress}>
+          <Text style={styles.text}>+</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
+  add: {
+    bottom: 30, // default standard for material design
+    right: 30,
+    position: "absolute",
+    width: 56,
+    height: 56,
+    borderRadius: 56 / 2,
+    elevation: 5,
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#FF6E6E"
+  },
 
-    add: {
-
-        bottom: 30,            // default standard for material design
-        right: 30,
-        position: "absolute",
-        width: 56,
-        height: 56,
-        borderRadius: 56 / 2,
-        elevation: 5,
-        alignItems: "center",
-        alignContent: "center",
-        justifyContent: "center",
-        backgroundColor: "#FF6E6E",
-    },
-
-    text: {
-        fontSize: 28,
-        color: "white",
-    }
-
-})
+  text: {
+    fontSize: 28,
+    color: "white"
+  }
+});
 
 export default AddButton;
