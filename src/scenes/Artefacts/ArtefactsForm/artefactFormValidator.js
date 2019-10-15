98d319345ad911b2eec4559e874efe4e5957d813
nickangmc <nickang.mc@hotmail.com>
2019-10-15 17:15:19 +1100

    Refactored form validator logic

--- src/scenes/Artefacts/ArtefactsForm/artefactFormValidator.js
+++ src/scenes/Artefacts/ArtefactsForm/artefactFormValidator.js
@@ -1,54 +1,27 @@
 const Validator = require("validator");
 
-// check if field is not empty
-// ImageURI
-export const validateImage = async input => {
-
-    console.log("input is", input)
-
-    let error = ""
-    if (Validator.isEmpty(input)) {
-        error = "An Image of the artefact is required";
-    }
-    return error
-};
-
-// Title
-export const validateTitle = input => {
-
-    let error = ""
-    if (Validator.isEmpty(input)) {
-        error = "Title field is required";
-    }
-    return error
-};
-
-// Description
-export const validateDescription = input => {
-
-    let error = ""
-    if (Validator.isEmpty(input)) {
-        error = "Description field is required";
-    }
-    return error
-};
-
-// Category
-export const validateCategory = input => {
-
-    let error = ""
-    if (Validator.isEmpty(input)) {
-        error = "Category field is required";
-    }
-    return error
-};
-
-// date
-export const validateDate = input => {
-
-    let error = ""
-    if (Validator.isEmpty(input)) {
-        error = "Date field is required";
-    }
-    return error
+export const validator = (field, input) => {
+  let error;
+  if (!Validator.isEmpty(input)) return "";
+  switch (field) {
+    case "imageURI":
+      error = "An Image of the artefact is required";
+      break;
+    case "title":
+      error = "Title field is required";
+      break;
+    case "description":
+      error = "Description field is required";
+      break;
+    case "category":
+      error = "Category field is required";
+      break;
+    case "dateObtained":
+      error = "DateObtained field is required";
+      break;
+    default:
+      error = "Unknown data field is passed in validator";
+      break;
+  }
+  return error;
 };
