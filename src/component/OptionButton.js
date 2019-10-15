9ed0ea97e7863ac16459cfb849ac123adc00e79f
Shaun Soong <shaunsoongdidi@gmail.com>
2019-10-16 10:11:00 +1100

    merged from masters, dynamic option button added

--- src/component/OptionButton.js
+++ src/component/OptionButton.js
@@ -18,15 +18,48 @@ class OptionButton extends Component {
         super(props);
     }
 
+    // only show options that have been included (remove blank spaces)
+    showButtons = () => {
+
+        var optionList = []
+
+        if (this.props.firstOption !== undefined) {
+            optionList.push(this.props.firstOption)
+        }
+        if (this.props.secondOption !== undefined) {
+            optionList.push(this.props.secondOption)
+        }
+        if (this.props.thirdOption !== undefined) {
+            optionList.push(this.props.thirdOption)
+        }
+        return optionList;
+    }
+
+    // only shows the functional toggles for each button available
+    toggleButtons = () => {
+        var toggleList = []
+
+        if (this.props.toggleFirstOption !== undefined) {
+            toggleList.push(this.props.toggleFirstOption)
+        }
+        if (this.props.toggleSecondOption !== undefined) {
+            toggleList.push(this.props.toggleSecondOption)
+        }
+        if (this.props.toggleThirdOption !== undefined) {
+            toggleList.push(this.props.toggleThirdOption)
+        }
+        return toggleList;
+    }
+
     render() {
         return (
-        <OptionsMenu
-            button={require("../../assets/images/icons/option.png")}
-            buttonStyle = {styles.icon}
-            destructiveIndex={1}            // only on ios (red indicator)
-            options={[this.props.firstOption, this.props.secondOption, this.props.thirdOption]}
-            actions={[this.props.toggleFirstOption, this.props.toggleSecondOption, this.props.toggleThirdOption]}
-        />
+            <OptionsMenu
+                button={require("../../assets/images/icons/option.png")}
+                buttonStyle={styles.icon}
+                destructiveIndex={1}            // only on ios (red indicator)
+                options={this.showButtons()}
+                actions={this.toggleButtons()}
+            />
         );
     }
 }
