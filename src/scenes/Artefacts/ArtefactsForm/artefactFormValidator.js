e1536dcdc75a8e69510726bfa7790805c2bb5419
nickangmc <nickang.mc@hotmail.com>
2019-10-16 18:24:27 +1100

    Fixed artefactForm validator issue with recognize current existing artefact photo issue

--- src/scenes/Artefacts/ArtefactsForm/artefactFormValidator.js
+++ src/scenes/Artefacts/ArtefactsForm/artefactFormValidator.js
@@ -1,11 +1,15 @@
 const Validator = require("validator");
 
-export const validator = (field, input) => {
+export const validator = (field, input, guard = null) => {
   let error;
   if (!Validator.isEmpty(input)) return "";
   switch (field) {
     case "imageURI":
-      error = "An Image of the artefact is required";
+      // for image, we use imageURI as guard in redux action to determine
+      // if we need to upload the current imageURI to GCS
+      // therefore, even if imageURI we need to check that there's no
+      // existing artefact photo to determine if this field is valid or not
+      if (!guard) error = "An Image of the artefact is required";
       break;
     case "title":
       error = "Title field is required";
